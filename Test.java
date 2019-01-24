d80558ffb3a68d536e0cd6fd02b47930e1c5ccf9
43252360 <43252360+Aidan-0@users.noreply.github.com>
2019-01-24 09:59:52 -0600

    Test
    Signed-off-by: 43252360 <43252360+Aidan-0@users.noreply.github.com>

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ class Test{
     System.out.println("Thingy");
     System.out.println("This is a thing(TM)");
     System.out.println("This thing is copyrighted(C)");
+    System.out.println("Thing(R)");
    }
 }
