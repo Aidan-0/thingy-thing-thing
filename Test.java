1785855596c02b416114afc7e5c0201b49a3d9c2
43252360 <43252360+Aidan-0@users.noreply.github.com>
2019-01-24 09:56:28 -0600

    Copyrighted

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ class Test{
   public static void main(String args[]){
     System.out.println("Thingy");
     System.out.println("This is a thing(TM)");
+    System.out.println("This thing is copyrighted(C)");
    }
 }
