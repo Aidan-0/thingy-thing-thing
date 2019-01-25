f2240347a1ee6d8a0c3229e36e9e3b905f7173a6
43252360 <43252360+Aidan-0@users.noreply.github.com>
2019-01-25 08:18:58 -0600

    Hi

--- Test.java
+++ Test.java
@@ -5,5 +5,6 @@ class Test{
     System.out.println("This thing is copyrighted(C)");
     System.out.println("Thing(R)");
     System.out.println("If this isn't verified");
-   }
+    System.out.println("VERIFY");  
+  }
 }
