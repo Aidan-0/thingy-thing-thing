943655dbd00723fcc86cf35d1857beb1094067de
43252360 <43252360+Aidan-0@users.noreply.github.com>
2019-01-25 18:15:24 -0600

    Added Whitespace to Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Test{
-  public static void main(String args[])
-  {
+  public static void main(String args[]){
+
     System.out.println("Thingy");
     System.out.println("This is a thing(TM)");
     System.out.println("This thing is copyrighted(C)");
@@ -9,5 +9,6 @@ class Test{
     System.out.println("VERIFY");
     System.out.println("Heck yeah now we're verified"); //But is it?
     //doot doot
+    
   }
 }
